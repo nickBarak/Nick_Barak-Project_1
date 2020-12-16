80ef0d2418130349ee506d12c34a260c2df24ae1
nickBarak <nicholasjbarak@gmail.com>
2020-12-16 03:17:38 -0800

    linking to remote tomcat server

--- src/resources.ts
+++ src/resources.ts
@@ -1,4 +1,5 @@
-export const API = 'http://localhost:8080/server';
+// export const API = 'http://localhost:8080/server';
+export const API = 'http://54.193.1.233:8080/server';
 
 export const routes = [
     '/',
