c68068fad2cc54eba613cd7721fcedb9e8880323
nickBarak <nicholasjbarak@gmail.com>
2020-12-16 16:08:17 -0800

    .

--- src/resources.ts
+++ src/resources.ts
@@ -1,6 +1,6 @@
 // export const API = 'http://localhost:8080/server';
 // export const API = 'http://54.193.1.233:8080/server';
-export const API = 'http://http://ec2-54-193-1-233.us-west-1.compute.amazonaws.com:8080/server';
+export const API = 'http://ec2-54-193-1-233.us-west-1.compute.amazonaws.com:8080/server';
 
 export const routes = [
     '/client',
